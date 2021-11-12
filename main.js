e9b422a5cb17749919de1a274c6b831b6d04cdbd
Victormbaabu <victor.mbaabu@strathmore.edu>
2021-11-12 16:36:12 +0300

    Added arrays and variables to the project

---
+++ main.js
@@ -0,0 +1,23 @@
+let bcentury = prompt("Enter your birth century")
+
+let gender = prompt("Enter your gender")
+
+let byear = prompt("Enter year born")
+
+let bmonth = parseInt(prompt("Enter birth month"))
+
+let bday = parseInt(prompt("Enter date of birth"))
+
+
+
+let mdays =['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+let fdays =['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+let mAkan =['kwasi','kwadwo','kwabena','kwaku','yaw','kofi','kwame']
+let fAkan =['akosua','adwoa','abenaa','akua','yaa','afua','ama']
+
+
+
+function calculate() {
+ 
+      console.log(`your Akan name is ${akan}. The day you were born is ${day}. `)
+}
