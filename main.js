4a4aa1b4e5c7cb57c0a14fb6d1160cb2f68b1f58
Victormbaabu <victor.mbaabu@strathmore.edu>
2021-11-15 00:39:40 +0300

    debugging functions in Js files

--- main.js
+++ main.js
@@ -1,46 +1,88 @@
-// let bcentury = prompt("Enter your birth century")
-
-// let gender = prompt("Enter your gender")
-
-// let byear = prompt("Enter year born")
-
-// let bmonth = parseInt(prompt("Enter birth month"))
-
-// let bday = parseInt(prompt("Enter date of birth"))
-
-
-// let mdays =['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
-
-// let fdays =['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
-
-// let mAkan =['kwasi','kwadwo','kwabena','kwaku','yaw','kofi','kwame']
-
-// let fAkan =['akosua','adwoa','abenaa','akua','yaa','afua','ama']
-
-// const init = function () {
-//       document.getElementById('submitbtn').addEventListener('click', submitfunc);
-// }
-
-// function submitfunc (ev) {
-//       ev.preventDefault();
-//       document.getElementById('form-user').reset
-// }
-
-// function checkAkan() {
-     
-//       console.log(`your Akan name is ${akan}. The day you were born is ${day}. `)
-
-     
-// }
+let daysOfWeek = [
+      "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+  
+    let maleAkanNames = [
+      "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+    ];
+  
+    let femaleAkanNames = [
+      "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+    ];
 
 
-let submtform = document.getElementById('form-user');
-let centurborn = document.getElementById('CC');
-let yearborn = document.getElementById('YY');
-let monthborn = document.getElementById('MM');
-let dateborn = document.getElementById('DD');
-let gender = document.getElementById('gender');
+    if (dayOfWeekNumber == 0){
+      index = 6;
+    } else {
+      index = dayOfWeekNumber - 1;
+    }
+  
+    console.log(index);
+  
 
-function dayfinder () {
-      alert(centurborn.value);
-}
+
+
+  function dayfinder () {
+      let yearOfBirth = document.getElementById("YYYY").value;
+      let monthOfBirth = Number(document.getElementById("MM").value);
+      let dayOfBirth = Number(document.getElementById("DD").value);
+      let genders = document.getElementsByName("gender");
+    
+      // function to get gender
+      function getGender () {
+        for (let gender of genders){
+          if (gender.checked){
+            return gender.value;
+          }
+        }
+      }
+    
+      let myGenderValue = getGender();
+      console.log(myGenderValue);
+    
+      // validation functions
+      function monthValidator () {
+        if (monthOfBirth < 1 || monthOfBirth > 12) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    
+      function dayValidator () {
+        if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+          if (dayOfBirth > 28 || dayOfBirth < 1) {
+            return false;
+          } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+            return false;
+          } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+            return false;
+          } else {
+            return true;
+          }
+        } else if (dayOfBirth < 1 || dayOfBirth > 31){
+          return false;
+        } else {
+          return true;
+        }
+      }  
+
+
+      if (myGenderValue == "male" && monthValid && dayValid) {
+            document.getElementById('result').textContent = "You were born on a " + daysOfWeek[index] + " , your Akan name is " + maleAkanNames[index];
+            document.getElementById('display-name').textContent = "Here is your Akan name: ";
+            document.getElementById('result').style.fontSize = "18px";
+            document.querySelector('h1').textContent = "Hello" + " " + maleAkanNames[index];
+            return false;
+          } else if (myGenderValue == "female" && monthValid && dayValid) {
+            document.getElementById('result').textContent = "You were born on a " + daysOfWeek[index] + " , your Akan name is " + femaleAkanNames[index];
+            document.getElementById('display-name').textContent = "Here is your Akan name: ";
+            document.getElementById('result').style.fontSize = "18px";
+            document.querySelector('h1').textContent = "Hello" + " " + femaleAkanNames[index];
+            return false;
+          } else {
+            alert("You entered an invalid day or month, please try again");
+          }
+      }
+
+
